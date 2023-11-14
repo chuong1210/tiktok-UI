2c715fd43e3c153c97a425f1b40bc5f2cf6efe51
chuong1210 <chuongvo1012@gmail.com>
2023-11-14 22:16:27 +0700

    xây dựng UI phần header#1

---
+++ src/assests/image/index.js
@@ -0,0 +1,5 @@
+const images = {
+    logo: require('~/assests/image/logo.svg').default,
+};
+
+export default images;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,11 +1,30 @@
+/* eslint-disable jsx-a11y/alt-text */
 import styles from './Header.module.scss';
 import classnames from 'classnames/bind';
+import images from '~/assests/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 const cx = classnames.bind(styles);
 function Header() {
-    console.log(cx);
+    console.log(images);
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <img src={images.logo} />
+                <div className={cx('search')}>
+                    <input type="text" placeholder="Search account and videos" spellCheck={true} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon icon={faSpinner} className={cx('loading')} />
+
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+
+                <div className={cx('actions')}></div>
+            </div>
         </header>
     );
 }
